1aec4b3770622e61eb980f7cf443f142c29f9940
Abhishek Kumar Dwivedi <abhishek.dwivedi@hexaviewtech.com>
2024-06-05 18:04:28 +0530

    Running groups using testng.xml

--- AutomationFramework/src/test/java/org/example/parallelTest.java
+++ AutomationFramework/src/test/java/org/example/parallelTest.java
@@ -3,20 +3,18 @@ import org.testng.annotations.Test;
 
 public class parallelTest {
 
-@Test(priority = 1,groups = {"sanity"})
+@Test(groups = {"sanity"})
 public void test1(){
             System.out.println("test1");
         }
 
-@Test(priority = 0,groups = {"regression"})
+@Test(groups = {"regression"})
 public void test2(){
             System.out.println("test2");
         }
 
-@Test(priority = 2,dependsOnMethods = "test1")
-public void test3(){
-            System.out.println("test3");
-        }
+@Test(groups = {"sanity","regression"})
+public void test3(){System.out.println("test3");}
 
 }
 
