724b9d92f55dae52769cca5965f1401a08f929aa
AniketSingh9897 <yuviyuvi338@gmail.com>
2024-06-13 16:01:19 +0530

    frames addition

--- src/main/java/com/ui/examples/BrowserUtil.java
+++ src/main/java/com/ui/examples/BrowserUtil.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,78 +17,80 @@ public abstract class BrowserUtil {
 	public BrowserUtil(WebDriver wd) {
 		super();
 		this.wd = wd;
-		wait= new WebDriverWait(wd,Duration.ofSeconds(30)); 
+		wait = new WebDriverWait(wd, Duration.ofSeconds(30));
 	}
 
 	public void goToWebsite(String url) {
 		wd.get(url);
+	}
+
+	public void maximizewindow() {
+		wd.manage().window().maximize();
+	}
+
+	public void enterTextInto(By locator, String textToEnter) {
+		// sleepFor(2);
+		// WebElement element=wd.findElement(locator);
+		// use below one
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		element.clear();
+		// sleepFor(2);
+		element.sendKeys(textToEnter);
+
+	}
+
+	public void clickOn(By locator) {
+
+		// WebElement element=wd.findElement(locator);
+		// use below one
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+		element.click();
+
+	}
+
+	public void getVisibleText(By locator) {
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		String count = element.getText();
+		System.out.println("No is " + count);
+
+	}
+
+	// handling alert and accept alert....
+	public void acceptAlert() {
+		Alert myAlert = wd.switchTo().alert();
+		myAlert.accept();
+	}
+
+	public void dismissAlert() {
+		Alert myAlert = wd.switchTo().alert();
+		myAlert.dismiss();
+	}
+
+	public String getAlertText() {
+		Alert myAlert = wd.switchTo().alert();
+		String textOfAlert = myAlert.getText();
+		return textOfAlert;
+
+	}
+
+	public void sleepFor(int seconds) {
+
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		public void maximizewindow() {
-			wd.manage().window().maximize();
-		}
-		
-		public void enterTextInto(By locator,String textToEnter)
-		{
-			//sleepFor(2);
-			//WebElement element=wd.findElement(locator);
-			//use below one
-			WebElement element=wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-			element.clear();
-			//sleepFor(2);
-			element.sendKeys(textToEnter);
-			
-		}
-		public void clickOn(By locator)
-		{
-			
-			//WebElement element=wd.findElement(locator);
-			//use below one
-			WebElement element=wait.until(ExpectedConditions.elementToBeClickable(locator));
-			element.click();
-			
-		}
-		
-		public void getVisibleText(By locator)
-		{
-			WebElement element=wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-			String count=element.getText();
-			System.out.println("No is " +count);
-			
-			
-		}
-		// handling alert and accept alert....
-		public void acceptAlert()
-		{
-			Alert myAlert= wd.switchTo().alert();
-			myAlert.accept();
-		}
-		public void dismissAlert()
-		{
-			Alert myAlert= wd.switchTo().alert();
-			myAlert.dismiss();
-		}
-		public String getAlertText()
-		{
-			Alert myAlert= wd.switchTo().alert();
-			String textOfAlert=myAlert.getText();
-			return textOfAlert;
-			
-			
-		}
-		
-		
-		
-		
-		/*public void sleepFor(int seconds)
-		{
-			
-			try {
-				Thread.sleep(seconds*1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}*/
+	}
+
+	public void switchToFrame(String frameIdOrName) {
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameIdOrName));
+	}
+	public void switchToFrame(int frameIndex) {
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameIndex));
+	}
+	
+	
 	
 
 }
--- src/main/java/com/ui/scenarios/HandlingFrame.java
+++ src/main/java/com/ui/scenarios/HandlingFrame.java
@@ -13,11 +13,14 @@ public class HandlingFrame {
 		wd.get("http://139.59.91.96:5001/selenium-workbook/wyswyg-editor.html");
 		WebElement headingLocator=wd.findElement(By.tagName("h1"));
 		String HeadingText=headingLocator.getText();
-		System.out.println(HeadingText);
+		System.out.println(HeadingText);// switch to frame
+		System.out.println("under frame");
 		wd.switchTo().frame("editor_ifr");
 		WebElement bodyOfTextArea=wd.findElement(By.id("tinymce"));
 		bodyOfTextArea.clear();
 		bodyOfTextArea.sendKeys("text of frame body");
+		wd.switchTo().parentFrame(); // exit from child frame and switch back to main/parent frame
+		System.out.println("Outside"+HeadingText);
 		
 		
 		
